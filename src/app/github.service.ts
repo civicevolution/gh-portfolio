39cb18a498c247cfede216653e04b551eb403a4e
Brian Sullivan <brian@civicevolution.org>
2021-12-07 11:08:39 -0800

    Added Orgnaizations

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 import { User } from './user';
 import { Repository } from './repository';
+import { Organization } from './organization';
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +23,10 @@ export class GithubService {
 
   getRepos(): Observable<Repository[]> {
     return this.http.get<Repository[]>(this.userUrl + '/repos');
+  }
+
+  getOrganizations(): Observable<Organization[]> {
+    return this.http.get<Organization[]>(this.userUrl + '/orgs');
   }
 
 }
