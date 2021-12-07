7d01cf84c12bae7de55c11f2d17bc0d72aeb9e47
Brian Sullivan <brian@civicevolution.org>
2021-12-07 10:56:35 -0800

    Added Repositories

---
+++ src/app/github.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+import { User } from './user';
+import { Repository } from './repository';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  private userUrl: string = '';
+
+  constructor(private http: HttpClient) {
+    this.userUrl = `${environment.apiUrl}/users/${environment.username}`;
+  }
+
+  getUser(): Observable<User> {
+    return this.http.get<User>(this.userUrl);
+  }
+
+  getRepos(): Observable<Repository[]> {
+    return this.http.get<Repository[]>(this.userUrl + '/repos');
+  }
+
+}
